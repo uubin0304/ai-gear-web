4f56cf4fc04d5f157be42d01042c059be58c47c1
uubin0304 <uubin0304@gmail.com>
2026-02-08 03:26:00 +0900

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {}, // 👈 v3 설정을 읽어주는 친구
+    autoprefixer: {}, // 👈 브라우저 호환성 맞춰주는 친구
   },
 };
 
